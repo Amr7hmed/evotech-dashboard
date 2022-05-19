47c9f49f3c94c49e74c4ef9fa1004a142df5033d
Amr AHMED <a.ahmed@ongoing-eg.com>
2022-05-19 16:01:19 +0200

    Update Data React Project

--- src/pages/staticcontent/navbar/index.jsx
+++ src/pages/staticcontent/navbar/index.jsx
@@ -10,14 +10,55 @@ import {
   faSave,
 } from "@fortawesome/free-regular-svg-icons";
 import { MyInputField } from "../../../components/field/index.jsx";
-
+import Spinner from "../../../components/spinner/Spinner.jsx";
 
 function StaticNavbar() {
+  const success = () => {
+    message.success("You edited Content");
+  };
+
   const location = useLocation();
   const group = location.pathname.substring(1);
 
   const [loading, setLoading] = useState(true);
   const [response, setResponse] = useState("");
+
+  const [serverMsg, setServerMsg] = useState("");
+  const [formLoading, setFormLoading] = useState(false);
+  const [isEdit, setIsEdit] = useState(true);
+
+  const Editeaction = function name(params1, params2, params3) {
+    setFormLoading(true);
+    setIsEdit(!isEdit);
+    const options = {
+      method: "put",
+      url: `http://dev1.evotech-co.com/api/admin/static-content/${params3}`,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json;charset=UTF-8",
+        Authorization: `Bearer ${JSON.parse(localStorage.getItem("token"))}`,
+      },
+      data: {
+        text: {
+          ar: params2.ar,
+          en: params2.en,
+        },
+      },
+    };
+    axios(options)
+      .then(function (response) {
+        let Create = async function () {
+          setFormLoading(false);
+          success();
+        };
+        Create();
+        return response;
+      })
+      .catch((error) => {
+        setLoading(false);
+        message.error(`somthing wrong , ${error}`);
+      });
+  };
 
   useEffect(() => {
     const options = {
@@ -41,11 +82,13 @@ function StaticNavbar() {
       });
   }, [group, localStorage.getItem("token")]);
 
+
+  
   return (
     <main>
       <div className="container">
         {loading ? (
-          <div> loading ... </div>
+        <Spinner />
         ) : (
           <div className="staticcontent__forms d-flex flex-column p-3 m-3">
             {response.map((items) => (
@@ -54,67 +97,84 @@ function StaticNavbar() {
                   initialValues={{}}
                   onSubmit={(values, actions) => {
                     console.log(values);
+                    Editeaction(items.key, values, items.id);
                   }}
                 >
                   {(FormikProps) => (
-                      <>
+                    <>
                       <Form>
                         <div className="row g-3">
-                          <div className="fielddiv">
+                          <div className="staticcontent__field ">
                             <span className="keyboard">
                               <FontAwesomeIcon icon={faKeyboard} />
                             </span>
-  
-                            <span className="edit">
-                              <FontAwesomeIcon icon={faEdit} />
-                            </span>
-  
-                            <span className="save">
-                              <FontAwesomeIcon icon={faSave} />
-                            </span>
-  
+
+
                             <MyInputField
-                              name={items.key}
+                              name={items.text.en}
                               type="text"
                               placeholder={`En ${items.key}`}
                               onChange={FormikProps.handleChange("en")}
-                              value={FormikProps.values[items.text.en]}
-                              onBlur={FormikProps.handleBlur}
+                              value={
+                                  isEdit
+                                  ? [items.text.en]
+                                  : FormikProps.values[items.text.en]
+                                }
+                                onBlur={FormikProps.handleBlur}
                             />
-                            
+                            {isEdit ? (
+                              <span className="edit"
+                              onClick={() => setIsEdit(!isEdit)}>
+                                <FontAwesomeIcon icon={faEdit} />
+                              </span>
+                            ) : (" "
+                            )}
+
+                            <button className="btn btn-success" type="submit">
+                              {formLoading ? <Spinner /> : 
+                              <FontAwesomeIcon icon={faSave} />}
+                            </button>
                           </div>
                         </div>
                       </Form>
 
-                      
-                    <Form>
-                      <div className="row g-3">
-                        <div className="fielddiv">
-                          <span className="keyboard">
-                            <FontAwesomeIcon icon={faKeyboard} />
-                          </span>
+                      <Form>
+                        <div className="row g-3">
+                          <div className="staticcontent__field">
+                            <span className="keyboard">
+                              <FontAwesomeIcon icon={faKeyboard} />
+                            </span>
 
-                          <span className="edit">
-                            <FontAwesomeIcon icon={faEdit} />
-                          </span>
 
-                          <span className="save">
-                            <FontAwesomeIcon icon={faSave} />
-                          </span>
+                            <MyInputField
+                              name={items.text.ar}
+                              type="text"
+                              placeholder={`Ar ${items.key}`}
+                              onChange={FormikProps.handleChange("ar")}
+                              onBlur={FormikProps.handleBlur}
+                              value={
+                                isEdit
+                                  ? [items.text.ar]
+                                  : FormikProps.values[items.text.ar]
+                              }
+                            />
+                            
+                            {isEdit ? (
+                              <span className="edit"
+                              onClick={() => setIsEdit(!isEdit)}>
+                                <FontAwesomeIcon icon={faEdit} />
+                              </span>
+                            ) : (" "
+                            )}
 
-                          <MyInputField
-                            name={items.key}
-                            type="text"
-                            placeholder={`Ar ${items.key}`}
-                            onChange={FormikProps.handleChange("ar")}
-                            value={FormikProps.values[items.text.ar]}
-                            onBlur={FormikProps.handleBlur}
-                          />
-                          
+                            <button className="btn btn-success" type="submit">
+                              {formLoading ? <Spinner /> : 
+                              <FontAwesomeIcon icon={faSave} />}
+                            </button>
+                          </div>
                         </div>
-                      </div>
-                    </Form>
-                      </>
+                      </Form>
+                    </>
                   )}
                 </Formik>
               </div>
